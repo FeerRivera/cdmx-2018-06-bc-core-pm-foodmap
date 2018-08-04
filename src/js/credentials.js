091fd3cbb4909aa169a8de24b6c4b596b99a4b5e
FeerRivera <feerkay15@gmail.com>
2018-08-04 16:47:47 -0500

    inicializacion con mapas

---
+++ src/js/credentials.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyA0I0RBDg7Tuw8IvcFyhQ76ks9Y6jA4ym8",
+  authDomain: "foodmaps-b7bc1.firebaseapp.com",
+  databaseURL: "https://foodmaps-b7bc1.firebaseio.com",
+  projectId: "foodmaps-b7bc1",
+  storageBucket: "foodmaps-b7bc1.appspot.com",
+  messagingSenderId: "387794680950"
+};
+firebase.initializeApp(config);
--- src/js/index.js
+++
@@ -1,7 +0,0 @@
-function timeout(){
-window.setTimeout("redirect()",5000)}
-
-function redirect(){
-window.location="food.htm"
-return}
-window.onload=timeout;
