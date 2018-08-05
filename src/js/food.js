7b9289046d83e95952dfbeac4d9bd588dd185bc4
FeerRivera <feerkay15@gmail.com>
2018-08-05 14:52:14 -0500

    nueva credencial

--- src/js/food.js
+++ src/js/food.js
@@ -1,10 +1,10 @@
 // Initialize and add the map
 function initMap() {
   // The location of Uluru
-  var uluru = {lat: 19.364456, lng: -99.187620};
+  let mex = {lat: 19.364456, lng: -99.187620};
   // The map, centered at Uluru
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: uluru});
+  let map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: mex});
   // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map});
+  let marker = new google.maps.Marker({position: mex, map: map});
 }
